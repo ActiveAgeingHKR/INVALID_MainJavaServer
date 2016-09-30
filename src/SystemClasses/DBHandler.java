4dc75fd27c8c29ad2bfe1d68b0148a61acb15045
chelsnola <nolascochelsi@gmail.com>
2016-09-30 21:09:41 +0200

    Added test prepared statements

--- src/SystemClasses/DBHandler.java
+++ src/SystemClasses/DBHandler.java
@@ -59,6 +59,104 @@ public class DBHandler {
         System.out.println("Connections is succesful :"+connectionEstablished);
         return connectionEstablished;
     }//end main
+    
+    //prepared statements for testing
+    public static boolean findEmployee(String userName, String tableName) {
+        boolean found = false;
+        try {
+            PreparedStatement findEmployee = connection.prepareStatement(
+                    "SELECT * FROM " + tableName + " WHERE emp_username=?;");
+            findEmployee.setString(1, userName);
+            ResultSet rs = findEmployee.executeQuery();
+            
+            while (rs.next()) {
+                if (userName.contentEquals(rs.getString("emp_username"))) {
+                    found = true;
+                } else {
+                    throw new Exception();
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        return found;
+    }
+    
+    public static boolean findCUfirstName(String firstName, String tableName) {
+        boolean found = false;
+        try {
+            PreparedStatement findEmployee = connection.prepareStatement(
+                    "SELECT * FROM " + tableName + " WHERE cu_firstname=?;");
+            findEmployee.setString(1, firstName);
+            ResultSet rs = findEmployee.executeQuery();
+            
+            while (rs.next()) {
+                if (firstName.contentEquals(rs.getString("cu_firstname"))) {
+                    found = true;
+                } else {
+                    throw new Exception();
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        return found;
+    }
+    
+    public static boolean findCUlastName(String lastName, String tableName) {
+        boolean found = false;
+        try {
+            PreparedStatement findEmployee = connection.prepareStatement(
+                    "SELECT * FROM " + tableName + " WHERE cu_lastname=?;");
+            findEmployee.setString(1, lastName);
+            ResultSet rs = findEmployee.executeQuery();
+            
+            while (rs.next()) {
+                if (lastName.contentEquals(rs.getString("cu_lastname"))) {
+                    found = true;
+                } else {
+                    throw new Exception();
+                }
+            }
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        return found;
+    }
+    
+    public static void addVisitor(String firstName, String lastName, String email,
+            int repeated) throws Exception {
+        
+        try {
+            PreparedStatement addVisitor = connection.prepareStatement("INSERT INTO visitors "
+            + "(vis_firstname, vis_lastname, vis_email, vis_repeated) VALUES (?, ?, ?, ?)");
+            
+            addVisitor.setString(1, firstName);
+            addVisitor.setString(2, lastName);
+            addVisitor.setString(3, email);
+            addVisitor.setInt(4, repeated);
+            addVisitor.execute();
+        } catch (SQLException ex) {
+            throw new Exception("Error");
+        }
+        
+    }
+    
+    public static void addCustomer(String firstName, String lastName, String address) throws Exception {
+        
+        try {
+            PreparedStatement addVisitor = connection.prepareStatement("INSERT INTO customers "
+            + "(cu_firstname, cu_lastname, cu_address) VALUES (?, ?, ?)");
+            
+            addVisitor.setString(1, firstName);
+            addVisitor.setString(2, lastName);
+            addVisitor.setString(3, address);
+            addVisitor.execute();
+        } catch (SQLException ex) {
+            throw new Exception("Error");
+        }
+        
+    }
 
 
 
